7df812a4ad2a9e593d7dd39bee5ece6a765c716d
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2021-11-15 00:53:02 +0100

    Images and layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby in TauIdeas",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
